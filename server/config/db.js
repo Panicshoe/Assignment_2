08f558cefb9bd107ccc2594ac67fbbc2bea5fe43
Panicshoe <86228058+Panicshoe@users.noreply.github.com>
2023-02-27 23:49:18 -0500

    Initial commit

---
+++ server/config/db.js
@@ -0,0 +1,10 @@
+// <!-- 
+//     Contact_me.ejs
+//     Name: Risav Bhaktaraj
+//     StudentID: 301313734
+//     Date: 02/27/2023 
+// -->
+module.exports =
+{
+    "URI":"mongodb://localhost/book_store"
+}
